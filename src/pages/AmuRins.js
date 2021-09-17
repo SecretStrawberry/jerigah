c4cb04dbbf03186b61f7ae860ed42be111876562
secretstrawberry <surduhori@gmail.com>
2021-09-17 14:57:57 +0200

    bla

--- src/pages/AmuRins.js
+++ src/pages/AmuRins.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Header from "./components/Header";
-import Navigation from "./components/Navigation.js";
+
 import Footer from "./components/Footer";
 import ClansContent from "./components/ClansContent";
 
--- src/pages/Nimbulans.js
+++ src/pages/Nimbulans.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Header from "./components/Header";
-import Navigation from "./components/Navigation";
+
 import Footer from "./components/Footer";
 import ClansContent from "./components/ClansContent";
 
