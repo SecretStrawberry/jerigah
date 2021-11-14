d4ecd7abf47d68a2ad5e9e7c9e446c3c8c2034bc
SecretStrawberry <surduhori@gmail.com>
2021-11-14 11:56:25 +0100

    resp under 500px

--- src/pages/components/Header.js
+++ src/pages/components/Header.js
@@ -1,18 +1,24 @@
 import React from "react";
 import Navigation from "./Navigation";
 
+import { Breakpoint, BreakpointProvider } from "react-socks";
+
 import "./Header.css";
 import "./glitch.scss";
 
 function Header(props) {
   return (
-    <header className="header" style={{ backgroundColor: props.background }}>
-      <Navigation />
-      <h1 className="header-h1 glitch" data-text={props.h1}>
-        {props.h1}
-      </h1>
-      <p>{props.paragraph}</p>
-    </header>
+    <BreakpointProvider>
+      <header className="header" style={{ backgroundColor: props.background }}>
+        <Breakpoint customQuery="(min-width: 501px)">
+          <Navigation />
+        </Breakpoint>
+        <h1 className="header-h1 glitch" data-text={props.h1}>
+          {props.h1}
+        </h1>
+        <p>{props.paragraph}</p>
+      </header>
+    </BreakpointProvider>
   );
 }
 
